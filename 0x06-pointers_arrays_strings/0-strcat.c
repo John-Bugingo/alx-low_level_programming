355d59444a9137608db8e1401ddb0fb26769bf10
John-Bugingo <johnbugingo12@gmail.com>
2022-03-24 06:51:02 -0700

    This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,13 @@
 char *_strcat(char *dest, char *src){
 	char *dest = "Hello ";
 	char *src = "Word!";
-	y
-		_putchar("concatenated String:%s\n", strcat(dest,src));
+	_purchar("%s\n", dest);
+	_purchar("%s\n",src);
+	_putchar("%s\n", strcat(dest,src));
+	_purchar("%s\n", src);
+	_purchar("%s". strcat(dest,src));
 	
 	}
+
 
 }
