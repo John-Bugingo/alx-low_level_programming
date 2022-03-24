5ee30de1ec95508bd986d17062f8513433b9c854
John-Bugingo <johnbugingo12@gmail.com>
2022-03-24 06:23:12 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src){
+	char *dest = "Hello ";
+	char *src = "Word!";
+	y
+		_putchar("concatenated String:%s\n", strcat(dest,src));
+	
+	}
+
+}
