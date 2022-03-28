de6f909dc20e02e9559cd96b56beea3dabd069ad
John-Bugingo <johnbugingo12@gmail.com>
2022-03-28 14:16:54 -0700

    More pointers, String, Arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,39 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-/**_memset - prints constant bytes
- * @s : the address of memmory to print
- *
- * Return: a pointer to the pointer s.
- */
- 
-	for (int i = 0; i<n; i++){
-		if (i%10)
-			{
-			_putchar((" ");
-			}
-		if (!(i % 10) && i)
-			{
-			_putchar("\n");
+	unsigned int i;
 
-			}
-		_putchar("0x%02x", s[i]);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
 	}
-	_putchar("\n");
 
-}
-/** main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-char s[98] = "b";
-_memset(s,98);
-_putchar("-");
-_putchar("\n");
-_memset(s,98);
-return (0);
+	return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-POINTERS, ARRAYS, AND STRINGS
+File 0-memset.c is a function that fills memory with a constant byte.
